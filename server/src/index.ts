d6e0c478cbd31ed3f6da2e4da498aad8b983585d
phogtr <phogbtr@gmail.com>
2021-06-18 23:34:07 -0700

    basic express server setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,21 @@
-console.log("hello world");
+import express from "express";
+import logger from "pino";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
+const log = logger({
+  prettyPrint: true,
+  base: {
+    pid: false,
+  }
+});
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  log.info(`Server started on ${PORT}`);  //  console.log is blocking IO, log from pino is not
+})
