20e69d3079786b4cc3816ca4e0673d6e9637e98b
phogtr <phogbtr@gmail.com>
2021-06-19 23:36:10 -0700

    update mongodb key

--- server/src/config/default.ts
+++ server/src/config/default.ts
@@ -1,4 +1,4 @@
 export default {
   dbURL:
-    "mongodb+srv://phogbt:abc12345abc@cluster0.amher.mongodb.net/react-post?retryWrites=true&w=majority",
+    "mongodb+srv://phogbt:123abc4567abc123@cluster0.amher.mongodb.net/react-post?retryWrites=true&w=majority",
 };
