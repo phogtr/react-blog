e0329db27cc951a40ecfc6857d3149b40fe3a082
phogtr <phogbtr@gmail.com>
2021-06-19 23:40:34 -0700

    updated mongodb key => secured

--- server/src/config/default.ts
+++
@@ -1,4 +0,0 @@
-export default {
-  dbURL:
-    "mongodb+srv://phogbt:123abc4567abc123@cluster0.amher.mongodb.net/react-post?retryWrites=true&w=majority",
-};
--- server/src/db/connect.ts
+++ server/src/db/connect.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import config from "../config/default";
+import config from "../config/key";
 import log from "../logger";
 
 const connect = () => {
