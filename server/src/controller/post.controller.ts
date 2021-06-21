4d37a5676ace0a539ab6783becab57a5b0a7f2f3
phogtr <phogbtr@gmail.com>
2021-06-21 02:14:19 -0700

    get all posts, update post, delete post

--- server/src/controller/post.controller.ts
+++ server/src/controller/post.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
-import { DocumentDefinition } from "mongoose";
+import { get } from "lodash";
+import { DocumentDefinition, FilterQuery, QueryOptions, UpdateQuery } from "mongoose";
 import Post, { PostDocument } from "../model/post.model";
 
 export async function createPostHandler(req: Request, res: Response) {
@@ -13,4 +14,50 @@ export async function createPostHandler(req: Request, res: Response) {
   const post = await Post.create(postObj);
 
   return res.send(post);
+}
+
+export async function getAllPostsHandler(req: Request, res: Response) {
+  const query: FilterQuery<PostDocument> = {};
+  const findOptions: QueryOptions = { lean: true };
+
+  const posts = await Post.find(query, {}, findOptions);
+
+  if (!posts) {
+    return res.sendStatus(404);
+  }
+  return res.send(posts);
+}
+
+export async function updatePostHandler(req: Request, res: Response) {
+  const postId = get(req, "params.postId");
+  const query: FilterQuery<PostDocument> = { postId };
+  const findOptions: QueryOptions = { lean: true };
+
+  const post = await Post.find(query, {}, findOptions);
+
+  if (!post) {
+    return res.sendStatus(404);
+  }
+
+  const update: UpdateQuery<PostDocument> = req.body;
+  const updateOptions: QueryOptions = { new: true };
+
+  const updatedPost = await Post.findOneAndUpdate(query, update, updateOptions);
+
+  return res.send(updatedPost);
+}
+
+export async function deletePostHandler(req: Request, res: Response) {
+  const postId = get(req, "params.postId");
+  const query: FilterQuery<PostDocument> = { postId };
+  const findOptions: QueryOptions = { lean: true };
+
+  const post = await Post.find(query, {}, findOptions);
+
+  if (!post) {
+    return res.sendStatus(404);
+  }
+
+  await Post.deleteOne(query);
+  return res.sendStatus(200);
 }
--- server/src/routes/posts.route.ts
+++ server/src/routes/posts.route.ts
@@ -1,7 +1,21 @@
 import { Express } from "express";
-import { createPostHandler } from "../controller/post.controller";
+import {
+  createPostHandler,
+  deletePostHandler,
+  getAllPostsHandler,
+  updatePostHandler,
+} from "../controller/post.controller";
 
 export default function (app: Express) {
-  // create post
+  // create a post
   app.post("/api/createPost", createPostHandler);
+
+  // get all posts
+  app.get("/api/getPosts", getAllPostsHandler);
+
+  // update a post
+  app.put("/api/post/:postId", updatePostHandler);
+
+  //delete a post
+  app.delete("/api/post/:postId", deletePostHandler);
 }
