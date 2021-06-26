e093083d1ff4720199835584cbc595f638289258
phogtr <phogbtr@gmail.com>
2021-06-25 23:11:52 -0700

    jwt verify (decode)

--- server/src/utils/jwt.utils.ts
+++ server/src/utils/jwt.utils.ts
@@ -6,3 +6,16 @@ const privateKey = config.privateKey as string;
 export function jwtSign(object: Object, options: jwt.SignOptions | undefined) {
   return jwt.sign(object, privateKey, options);
 }
+
+export function jwtDecode(token: string) {
+  try {
+    const decoded = jwt.verify(token, privateKey);
+    return { valid: true, expired: false, decoded };
+  } catch (error) {
+    return {
+      valid: false,
+      expired: error.message === "jwt expired",
+      decoded: null,
+    };
+  }
+}
