18048235bff6cfee14e8dbc23dc6feb45a08d164
phogtr <phogbtr@gmail.com>
2021-06-27 00:47:11 -0700

    get from lodash instead to actually get token

--- server/src/middleware/deserializeUser.ts
+++ server/src/middleware/deserializeUser.ts
@@ -1,16 +1,18 @@
+import { get } from "lodash";
 import { Request, Response, NextFunction } from "express";
 import { jwtDecode } from "../utils/jwt.utils";
 import { renewAccessToken } from "../controller/session.controller";
 
 const deserializeUser = async (req: Request, res: Response, next: NextFunction) => {
   // https://security.stackexchange.com/questions/108662/why-is-bearer-required-before-the-token-in-authorization-header-in-a-http-re
-  const accessToken = req.get("headers.authorization")?.replace(/^Bearer\s/, "");
-  const refreshToken = req.get("headers.x-refresh");
+  const accessToken = get(req, "headers.authorization", "").replace(/^Bearer\s/, "");
+  const refreshToken = get(req, "headers.x-refresh");
 
   if (!accessToken) return next();
 
   const { decoded, expired } = jwtDecode(accessToken);
 
+  // access token hasn't expired yet
   if (decoded) {
     // @ts-ignore
     req.user = decoded;
