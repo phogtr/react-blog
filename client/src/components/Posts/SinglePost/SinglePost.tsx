d6600ea4f99d8cdd7dd549451e16bea09b1d8a4e
phogtr <phogbtr@gmail.com>
2021-07-07 19:35:37 -0700

    handle url navigating singlePost

--- client/src/components/Posts/SinglePost/SinglePost.tsx
+++ client/src/components/Posts/SinglePost/SinglePost.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import axios from "src/config/axios";
 import { ReduxReducer } from "src/redux/configureStore";
 import { getSinglePostRequest } from "src/redux/ducks/posts/action";
-import { PostDataState } from "src/redux/ducks/posts/postsReducer";
+import { PostData, PostDataState } from "src/redux/ducks/posts/postsReducer";
 
 interface SinglePostProps {}
 
@@ -12,22 +13,27 @@ interface RouteParams {
 }
 
 export const SinglePost: React.FC<SinglePostProps> = () => {
-  // const [post, setPost] = useState<PostData>({ title: "", content: "" });
   const params = useParams<RouteParams>();
   const dispatch = useDispatch();
   const post: PostDataState = useSelector((state: ReduxReducer) => state.posts);
-  console.log(post);
 
-  // const getPost = async () => {
-  //   const res = await axios.get(`http://localhost:5000/api/post/${params.id}`);
-  //   setPost(res.data[0]);
-  // };
+  const [apiPost, setApiPost] = useState<PostData>({ title: "", content: "" }); // handle case of navigating through the url (redux state empty at this point)
+
+  // todo: move this to a separate file
+  const getPost = async () => {
+    const res = await axios.get(`http://localhost:5000/api/post/${params.id}`);
+    setApiPost(res.data[0]);
+  };
 
   useEffect(() => {
-    dispatch(getSinglePostRequest(params.id));
+    if (post.posts.length <= 0) {
+      getPost();
+    } else {
+      dispatch(getSinglePostRequest(params.id));
+    }
   }, []);
 
-  if (post.posts.length <= 0) {
+  if (!apiPost) {
     return (
       <div>
         <h1>Could not find this post</h1>
@@ -36,9 +42,19 @@ export const SinglePost: React.FC<SinglePostProps> = () => {
   }
 
   return (
-    <div>
-      <h1>{post.posts[0].title}</h1>
-      <div>{post.posts[0].content}</div>
-    </div>
+    <>
+      {post.posts.length === 1 ? (
+        <div>
+          <h1>{post.posts[0].title}</h1>
+          <div>{post.posts[0].content}</div>
+        </div>
+      ) : (
+        <div>
+          <h1>{apiPost.title}</h1>
+          <div>{apiPost.content}</div>
+          <h3>API Post</h3>
+        </div>
+      )}
+    </>
   );
 };
